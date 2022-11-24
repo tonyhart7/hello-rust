574747c9dad65d7f62607024f8cd3642e0e6adf8
Tony hart <tonyhart2020@gmail.com>
2022-11-24 21:48:25 +0700

    section hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,14 @@
-use std::io;
-use rand::Rng;
-use std::cmp::Ordering;
-
-
 fn main() {
-    println!("Guess the number!");
+    use std::collections::HashMap;
 
-    let secret_number = rand::thread_rng().gen_range(1..=100);
+    let text = "hello world wonderful world";
 
-    loop {
-        println!("Please input your guess.");
+    let mut map = HashMap::new();
 
-    let mut guess = String::new();
-
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
-    
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-    println!("You guessed: {guess}");
-
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("Too small!!"),
-        Ordering::Greater => println!("Too BIG!!"),
-        Ordering::Equal => {
-            println!("You Win");
-            break;
-        } 
-            
+    for word in text.split_whitespace() {
+        let count = map.entry(word).or_insert(0);
+        *count += 1;
     }
-}
-    
+
+    println!("{:?}", map);
 }
