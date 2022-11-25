178313fc84d0575b438cfdce445ee68fa8211594
Tony hart <tonyhart2020@gmail.com>
2022-11-25 13:19:32 +0700

    trait course

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,29 @@
 fn main() {
-    use std::collections::HashMap;
+    let string1 = String::from("abcd");
+    let string2 = "xyz";
 
-    let text = "hello world wonderful world";
+    let result = longest_with_an_announcement(
+        string1.as_str(),
+        string2,
+        "Today is someone's birthday!",
+    );
+    println!("The longest string is {}", result);
+}
 
-    let mut map = HashMap::new();
+use std::fmt::Display;
 
-    for word in text.split_whitespace() {
-        let count = map.entry(word).or_insert(0);
-        *count += 1;
+fn longest_with_an_announcement<'a, T>(
+    x: &'a str,
+    y: &'a str,
+    ann: T,
+) -> &'a str
+where
+    T: Display,
+{
+    println!("Announcement! {}", ann);
+    if x.len() > y.len() {
+        x
+    } else {
+        y
     }
-
-    println!("{:?}", map);
 }
