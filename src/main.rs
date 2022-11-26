8d826e860b898cf24f350944f747c914814bd38a
Tony hart <tonyhart2020@gmail.com>
2022-11-27 02:16:07 +0700

    web server. end of rust programming book chapter

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,35 @@
-
-mod post;
-use post::Post;
+#![allow(unused)]
+use std::{
+    fs,
+    io::{prelude::*, BufReader},
+    net::{TcpListener, TcpStream},
+};
+// --snip--
 
 fn main() {
-    let mut post = Post::new();
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
 
-    post.add_text("I ate a salad for lunch today");
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
 
-    let post = post.request_review();
+        handle_connection(stream);
+    }
+}
 
-    let post = post.approve();
+fn handle_connection(mut stream: TcpStream) {
+    let buf_reader = BufReader::new(&mut stream);
+    let http_request: Vec<_> = buf_reader
+        .lines()
+        .map(|result| result.unwrap())
+        .take_while(|line| !line.is_empty())
+        .collect();
 
-    assert_eq!("I ate a salad for lunch today", post.content());
+    let status_line = "HTTP/1.1 200 OK";
+    let contents = fs::read_to_string("hello.html").unwrap();
+    let length = contents.len();
+
+    let response =
+        format!("{status_line}\r\nContent-Length: {length}\r\n\r\n{contents}");
+
+    stream.write_all(response.as_bytes()).unwrap();
 }
