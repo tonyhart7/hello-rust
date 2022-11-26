47639a13ef01802e811b68704e080557ba43144d
Tony hart <tonyhart2020@gmail.com>
2022-11-26 13:50:04 +0700

    OOP feature in rust

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,15 @@
-use std::sync::mpsc;
-use std::thread;
-use std::time::Duration;
+
+mod post;
+use post::Post;
 
 fn main() {
-    // --snip--
+    let mut post = Post::new();
 
-    let (tx, rx) = mpsc::channel();
+    post.add_text("I ate a salad for lunch today");
 
-    let tx1 = tx.clone();
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("hi"),
-            String::from("from"),
-            String::from("the"),
-            String::from("thread"),
-        ];
+    let post = post.request_review();
 
-        for val in vals {
-            tx1.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
+    let post = post.approve();
 
-    thread::spawn(move || {
-        let vals = vec![
-            String::from("more"),
-            String::from("messages"),
-            String::from("for"),
-            String::from("you"),
-        ];
-
-        for val in vals {
-            tx.send(val).unwrap();
-            thread::sleep(Duration::from_secs(1));
-        }
-    });
-
-    for received in rx {
-        println!("Got: {}", received);
-    }
-
-    // --snip--
+    assert_eq!("I ate a salad for lunch today", post.content());
 }
